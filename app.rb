5c01b133190076a7bc3bfc64734256ee0a43f0ef
Robert Tate <btate712@gmail.com>
2019-04-21 21:10:53 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -10,7 +10,9 @@ class App < Sinatra::Base
   end
 
   post '/puppy' do
-    binding.pry
+    @name = params[:name]
+    @breed = params[:breed]
+    @months_old = params[:months_old]
     erb :display_puppy
   end
 end
